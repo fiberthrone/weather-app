923eb4246417bf9e351b6467cd60c89d4ce649ad
Andrei Dotsenko <fiberthrone@gmail.com>
2021-06-05 14:39:13 +0300

    Get API key from query parameter

--- src/PageHome.js
+++ src/PageHome.js
@@ -1,5 +1,27 @@
+import { useEffect } from "react";
+import { useLocation, useHistory } from "react-router-dom";
+
+function useQuery() {
+  return new URLSearchParams(useLocation().search);
+}
+
 function PageHome() {
-  return "Hello world!";
+  const query = useQuery();
+  const history = useHistory();
+
+  const apiKey = query.get("api-key");
+
+  useEffect(() => {
+    if (!apiKey) {
+      history.replace("/not-found");
+    }
+  }, [apiKey]);
+
+  if (!apiKey) {
+    return null;
+  }
+
+  return `Hello world! API key: ${apiKey}`;
 }
 
 export default PageHome;
